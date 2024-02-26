b67415f9456d878ed0ef48e9b427846357d3c7f9
Brian Zavala <brianzav23@gmail.com>
2024-02-25 22:18:49 -0300

    adding new entitie in project

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,14 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/*public class PacienteController { TODO
+    @PutMapping
+    @Transactional
+            public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente)
+    var paciente = repository.getReferenceById(dados.id());
+    paciente.atualizarInformacoes(dados)
+}*/
---
+++ src/main/java/med/voll/api/paciente/PacienteRepository.java
@@ -0,0 +1,13 @@
+package med.voll.api.paciente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/*TODO
+public class PacienteRepository extends JpaRepository<Paciente, Long> {
+    @Override
+    public List<Paciente> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+}*/
